1205befa1f886b2298d424cb20ff195c0e17b6a4
Shynur <one.last.kiss@qq.com>
2022-09-29 12:46:02 +0800

    message

--- Y-combinator.lisp
+++ Y-combinator.lisp
@@ -6,7 +6,11 @@
                                   (apply (funcall x x) args)))))))
 
 (defparameter fib (funcall Y #'(lambda (f)
-                                 #'(lambda (n)
-                                     (if (<= n 1)
-                                         n
-                                         (+ (funcall f (- n 1)) (funcall f (- n 2))))))))
+                                 #'(lambda (n a b)
+                                     (if (zerop n)
+                                         a
+                                         (funcall f (1- n) b (+ a b)))))))
+
+(defun fib (n)
+  (dotimes (i n)
+    (format t "~a~%" (funcall fib i 0 1))))
