c7cdba433c0359d3e2e344a6482fd843e7eb60c2
Shynur <one.last.kiss@qq.com>
2022-10-03 02:48:44 +0800

    one commit

--- -spam-filter.lisp
+++ -spam-filter.lisp
@@ -1,4 +1,6 @@
 ;;;; Used Package: cl-ppcre <github.com/edicl/cl-ppcre>
+;;;; 1. Key Data:
+;;;; 2. Filter Trainer
 
 (defpackage :shynur.spam-filter
   (:documentation
@@ -13,6 +15,12 @@ The main function to implement has a simple job:
 
 (in-package :shynur.spam-filter)
 
+(defun classify (text)
+  (let ((score (score (extract-features text))))
+    (cond ((<= score *max-ham-score*)  'ham)
+          ((>= score *min-spam-score*) 'spam)
+          ('unsure))))
+
 ;;; Key Data ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; parameter: *max-ham-score* *min-spam-score* *features*
 
@@ -24,11 +32,16 @@ The main function to implement has a simple job:
 
 ;; API
 (defvar *features* (make-hash-table :test #'equal)
-  "a feature database. helper-function: clear-features, lookup-feature")
+  "a word-feature database")
 
 ;; API
 (defun clear-features ()
-  (setf *features* (make-hash-table :test #'equal)))
+  "clear all the results of training, including:
+     *features*, word frequency,
+     *total-hams* and *total-spams*"
+  (setf *features*    (make-hash-table :test #'equal)
+        *total-hams*  0
+        *total-spams* 0))
 
 (defun lookup-feature (word)
   "takes a word and returns the appropriate feature, creating it if necessary"
@@ -36,12 +49,6 @@ The main function to implement has a simple job:
       (setf (gethash word *features*) (make-instance 'word-feature :word word))))
 
 ;;; Core ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defun classify (text)
-  (let ((score (score (extra-features text))))
-    (cond ((<= score *max-ham-score*)  :ham)
-          ((>= score *min-spam-score*) :spam)
-          (:unsure))))
 
 (defclass word-feature ()
   ((word
@@ -61,7 +68,44 @@ The main function to implement has a simple job:
   (mapcar #'lookup-feature (delete-duplicates (cl-ppcre:all-matches-as-strings "[a-zA-Z]{3,}" text)
                                               :test #'string=)))
 
+(defmethod inc-count ((feature word-feature) type)
+  (ecase type
+    (ham  (incf (ham-count  feature)))
+    (spam (incf (spam-count feature)))))
+
+(defmethod P_ham ((feature word-feature))
+  (with-accessors ((hams ham-count)) feature
+    (/ hams (max 1 *total-hams*))))
+(defmethod P_spam ((feature word-feature))
+  (with-accessors ((spams spam-count)) feature
+    (/ spams (max 1 *total-spams*))))
+
 (defmethod print-object ((obj word-feature) stream)
   (print-unreadable-object (obj stream :type t)
     (with-accessors ((word word) (hams ham-count) (spams spam-count)) obj
       (format stream "~s, hams: ~d, spams: ~d" word hams spams))))
+
+;;; Filter Trainer ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; these two var can be CLEARED by function clear-features
+(defvar *total-hams*  0)
+(defvar *total-spams* 0)
+
+(defun train (text type)
+  "update: the hash-table of word-features,
+           the number of already counted hams or spams"
+  (dolist (feature (extract-features text))
+    (inc-count feature type))
+  (ecase type
+    (ham  (incf *total-hams*))
+    (spam (incf *total-spams*))))
+
+;;; Calculate Probability ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun bayesian-P_spam (feature &optional (assumed-P 1/2) (weight 1))
+  (let* ((P_ham  (P_ham  feature))
+         (P_spam (P_spam feature))
+         (P_ham+P_spam (+ P_ham P_spam))
+         (basic-P_spam (if (zerop P_ham+P_spam) 1/2 (/ P_spam P_ham+P_spam)))
+         (data-points (+ (ham-count feature) (spam-count feature))))
+    (/ (+ (* weight assumed-P) (* basic-P_spam data-points)) (+ weight data-points))))
